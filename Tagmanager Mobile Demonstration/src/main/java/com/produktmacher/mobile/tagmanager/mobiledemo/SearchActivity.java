2ed68836757be07b212b5f0eb7e94eb62040d58a
sla <stefan.langer@produktmacher.com>
2014-03-04 17:50:40 +0100

    started with search activity

--- Tagmanager Mobile Demonstration/src/main/java/com/produktmacher/mobile/tagmanager/mobiledemo/SearchActivity.java
+++ Tagmanager Mobile Demonstration/src/main/java/com/produktmacher/mobile/tagmanager/mobiledemo/SearchActivity.java
@@ -1,13 +1,9 @@
 package com.produktmacher.mobile.tagmanager.mobiledemo;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class SearchActivity extends ActionBarActivity {
 
@@ -15,12 +11,6 @@ public class SearchActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -42,22 +32,6 @@ public class SearchActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-            return rootView;
-        }
     }
 
 }
